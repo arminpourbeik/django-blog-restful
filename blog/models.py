476e96d2c2062e87d6b4eb8745114e3cc3c8b65b
Armin <armin.pourbeik@gmail.com>
2021-01-15 02:58:09 +0330

    Create blog model

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,53 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+
+class Category(models.Model):
+    """
+    Database model for category
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    """
+    Database model for post
+    """
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status="published")
+
+    OPTIONS = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    category = models.ForeignKey(
+        to=Category, on_delete=models.PROTECT(), related_name="posts"
+    )
+    content = models.TextField()
+    slug = models.SlugField(max_length=255, unique_for_date="created_at")
+    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    status = models.CharField(max_length=10, choices=OPTIONS, default="published")
+
+    objects = models.Manager()  # Default manager
+    published = PostObjects()  # Custom manager
+
+    class Meta:
+        ordering = ("-published",)
+
+    def __str__(self) -> str:
+        return self.title
